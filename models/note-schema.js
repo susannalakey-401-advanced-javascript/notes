e318cfc83c8bde72fa44ad63f4c975712cd318b0
Susanna Lakey <susannalakey@gmail.com>
2020-02-12 20:03:13 -0800

    database storing notes

--- models/note-schema.js
+++ models/note-schema.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const noteSchema = mongoose.Schema({
   action: { type: String, required: true },
   payload: { type: String, required: true },
+  category: { type: String, required: false },
 });
 
 
